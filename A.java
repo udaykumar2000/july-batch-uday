da36dbb3d90ca147816e36c13d91ea7354ae5e72
udaykumar2000 <112633483+udaykumar2000@users.noreply.github.com>
2022-09-14 19:53:24 +0530

    comitted
    comitted changes

---
+++ A.java
@@ -0,0 +1,12 @@
+class A 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("main begin");
+		for(int i = 0; i <= 5; i++)
+		{
+			System.out.println("loop: " + i);
+		}
+		System.out.println("main end");
+	}
+}
